07af49e57987dd76600842725d86309689c2ec80
mustafa-mahmoud203 <must.afa000262@gmail.com>
2024-04-01 13:50:59 +0200

    create  end point to add user addresses

---
+++ src/routes/addresses.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as controllers from "../controllers/addresses.js";
+// import * as validators from "../validators/favorites.js";
+import { auth, isAllowedTo } from "../middleware/auth.js";
+
+const router = Router();
+
+router.use(auth, isAllowedTo("user"));
+
+router.route("/").post(controllers.addUserAddress);
+//   .get(controllers.getLoggedUserAddresses);
+
+// router.delete("/:productId", controllers.removeUserAddress);
+
+export default router;
