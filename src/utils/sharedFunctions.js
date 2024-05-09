cbc52ed016d105ae886bf88c3dc84b743302aaf3
mustafa-mahmoud203 <must.afa000262@gmail.com>
2024-05-09 16:27:23 +0300

    refactor

--- src/utils/sharedFunctions.js
+++ src/utils/sharedFunctions.js
@@ -2,7 +2,11 @@
 
 const chackImage = (doc) => {
   if (doc.image) {
-    const imageURL = `${process.env.DEV_BADED_URL}/uploads/${doc.image}`;
+    const appModeEnv =
+      process.env.NODE_ENV == "production"
+        ? process.env.PROD_BADED_URL
+        : process.env.DEV_BADED_URL;
+    const imageURL = `${appModeEnv}/uploads/${doc.image}`;
     doc.image = imageURL;
   }
   if (doc.images) {
